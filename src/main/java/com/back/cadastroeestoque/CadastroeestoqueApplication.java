d74276d2d63d2db781e4f2102a6e0af5cf597593
Edson <edson_schmitz_@hotmail.com>
2026-06-04 14:00:22 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/back/cadastroeestoque/CadastroeestoqueApplication.java
@@ -0,0 +1,13 @@
+package com.back.cadastroeestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroeestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroeestoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/cadastroeestoque/CadastroeestoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.cadastroeestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroeestoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
